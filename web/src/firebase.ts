8c277c4204e836eb83aa8598f65465db16abd958
fernando-ruans <fernando_ruans@hotmail.com>
2025-05-30 19:30:23 -0300

    feat: atualizar configuração do Firebase para usar variáveis de ambiente

--- web/src/firebase.ts
+++ web/src/firebase.ts
@@ -7,12 +7,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {  apiKey: "AIzaSyCd8KdHRhK9hPVbHv49OGMrjYnzyhie02I",
-  authDomain: "deliveryx-c0ea6.firebaseapp.com",
-  projectId: "deliveryx-c0ea6",
-  storageBucket: "deliveryx-c0ea6.appspot.com",  messagingSenderId: "539644821489",
-  appId: "1:539644821489:web:a4627d8f551b45dcd50e50",
-  measurementId: "G-80WLFQ5HJT"
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
